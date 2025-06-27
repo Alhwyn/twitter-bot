484c3902027fc1e30b5aeede2c022e2c4a83953c
alhwyn <alhwyndwyne0411@gmail.com>
2025-06-27 01:29:49 -0700

    BRING IT ONNgit add .git add .git add . Problems: 107

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
-pub use self::api::{TwitterApi, TwitterApiWithUserCtx};
+#[cfg(feature = "oauth2")]
+pub extern crate oauth2;
 
-pub mod auth;
+pub mod api;
+pub mod api_result;
+pub mod authorization;
+pub mod data;
+pub mod error;
+pub mod id;
+pub mod meta;
+pub mod query;
+pub mod requests;
+mod utils;
+
+pub use self::{
+    api::{TwitterApi, TwitterApiWithUserCtx},
+    api_result::{ApiError, ApiPayload, ApiResponse, ApiResult},
+    authorization::Authorization,
+    data::{Media, Place, Poll, Space, Tweet, User},
+    error::{Error, Result},
+};
+
+pub mod prelude {
+    pub use crate::api_result::PaginableApiResponse;
+    pub use crate::authorization::Authorization;
+    pub use crate::id::{IntoNumericId, IntoStringId};
+    pub use crate::meta::PaginationMeta;
+}
